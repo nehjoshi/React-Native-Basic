f9c08b82bb323fe1d322fa0a54bec303ecae507a
Neh Joshi <nehjoshi5@gmail.com>
2021-11-21 17:42:19 +0530

    Flat list

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('');
-  const [ready, setReady] = useState(false);
-  const handleSubmit = () => {
-    alert(`Hello ${name}`);
-    setReady(true);
-  }
+  const [people, setPeople] = useState([
+    { name: "Neh", age: "23", id: 1 },
+    { name: "David", age: "24", id: 2 },
+    { name: "Scott", age: "25", id: 3 },
+    { name: "Joe", age: "26", id: 4 },
+    { name: "Dan", age: "27", id: 5 },
+    { name: "Kinsey", age: "29", id: 6 },
+    { name: "Otis", age: "30", id: 7 },
+    { name: "Ruby", age: "31", id: 8 },
+    { name: "Maeve", age: "32", id: 9 },
+    { name: "Eric", age: "33", id: 10 },
+    { name: "Dude", age: "34", id: 11 },
+    { name: "Michael", age: "14", id: 12 },
+    { name: "Elevel", age: "32", id: 13 },
+    { name: "Love", age: "22", id: 14 },
+    { name: "Theo", age: "23", id: 15 },
+  ]);
   return (
     <View style={styles.container}>
-        <Text style={styles.headerText}>
-          Enter your name:
-        </Text>
-        <TextInput onChangeText={(text) => setName(text)} style={styles.input} />
-        <Button title="Submit" onPress={handleSubmit} />
-        {ready && <Text style={styles.headerText}>Hello {name}</Text>}
+      <FlatList   //Flatlist is more efficient than 
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.text}>{item.name} : {item.age}</Text>
+        )}
+      />
+      {/* <ScrollView>
+          {people.map(item => (
+              <View key={item.id}>
+                <Text style={styles.text}>{item.name} : {item.age}</Text>
+              </View>
+            )
+          )}
+        </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -27,20 +48,19 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
-  input: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: "#0081fa",
+    elevation: 10,
     padding: 10,
-  },
-  headerText: {
-    fontSize: 30,
-    color: 'gray',
+    marginBottom: 10,
+    marginHorizontal: 2,
+    marginVertical: 5,
+    color: '#fff',
   }
 });
